d7851b4a512680dcb069de35c249ddea5cd816a3
Jackson <jackmiras@gmail.com>
2015-11-02 19:27:56 -0300

    Refatorando a código e o layout da splash screen

--- app/src/main/java/com/fullscreen/SplashScreenActivity.java
+++ app/src/main/java/com/fullscreen/SplashScreenActivity.java
@@ -1,27 +1,28 @@
 package com.fullscreen;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.Timer;
-import java.util.TimerTask;
 import com.example.webview.R;
 
-public class SplashScreenActivity extends ActionBarActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash_screen);
+        if (getSupportActionBar() != null)
+            getSupportActionBar().hide();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
 
         new Timer().schedule(new TimerTask() {
@@ -29,29 +30,21 @@ public class SplashScreenActivity extends ActionBarActivity {
             @Override
             public void run() {
                 finish();
-
                 Intent intent = new Intent();
                 intent.setClass(SplashScreenActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         }, 4000);
-
-
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
